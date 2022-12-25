3f08c879aa46f39e33018b8fa3a6cfa29b54fc0f
Ciel <47144701+Shuaiouke@users.noreply.github.com>
2022-12-25 17:33:16 +0800

    feat: Added default option for sealed_typestate

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
 #[macro_export]
 macro_rules! sealed_typestate {
-    ($state:ident $sealed:ty { $($states:ident)* }) => {
+    ($state:ident { $($states:ident),* }) => {
+        pub trait $state: crate::sealed::Sealed {}
+        $(
+            pub enum $states {}
+            impl crate::sealed::Sealed for $states {}
+            impl $state for $states {}
+        )*
+    };
+    ($state:ident $sealed:ty { $($states:ident),* }) => {
         pub trait $state: $sealed {}
         $(
             pub enum $states {}
@@ -10,5 +18,16 @@ macro_rules! sealed_typestate {
     };
 }
 
+mod sealed {
+    pub trait Sealed {}
+}
+
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        sealed_typestate!(Side { Client, Server });
+    }
+}
