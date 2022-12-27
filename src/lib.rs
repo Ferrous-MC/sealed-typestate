e18ea34efce143c18f52db95991a37f64f6b9c9d
Ciel <47144701+Shuaiouke@users.noreply.github.com>
2022-12-27 20:09:32 +0800

    ci: Attempt to make compile tests work with Github action

--- src/lib.rs
+++ src/lib.rs
@@ -20,9 +20,9 @@ mod tests {
     #[test]
     fn test_compiles() {
         let t = TestCases::new();
-        t.pass("tests/01-default-sealed-trait.rs");
-        t.pass("tests/02-specified-sealed-trait.rs");
-        t.compile_fail("tests/03-default-sealed-trait-does-not-exist.rs");
-        t.compile_fail("tests/04-specified-sealed-trait-does-not-exist.rs");
+        t.pass("tests/compile-tests/01-default-sealed-trait.rs");
+        t.pass("tests/compile-tests/02-specified-sealed-trait.rs");
+        t.compile_fail("tests/compile-tests/03-default-sealed-trait-does-not-exist.rs");
+        t.compile_fail("tests/compile-tests/04-specified-sealed-trait-does-not-exist.rs");
     }
 }
