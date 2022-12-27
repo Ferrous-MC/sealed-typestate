9d225d20aabd1b952039a572603ed504c20096f7
Ciel <47144701+Shuaiouke@users.noreply.github.com>
2022-12-27 19:50:52 +0800

    refactor: Changed syntax for specifying the sealed trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 #[macro_export]
 macro_rules! sealed_typestate {
     ($state:ident { $($states:ident),* }) => {
-        sealed_typestate!($state crate::private::Sealed { $($states),* });
+        sealed_typestate!($state<crate::private::Sealed> { $($states),* });
     };
-    ($state:ident $sealed:path { $($states:ident),* }) => {
+    ($state:ident<$sealed:path> { $($states:ident),* }) => {
         pub trait $state: $sealed {}
         $(
             pub enum $states {}
@@ -13,21 +13,10 @@ macro_rules! sealed_typestate {
     };
 }
 
-mod private {
-    pub trait Sealed {}
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn specified_sealed_trait() {
-        sealed_typestate!(Side crate::private::Sealed { Client, Server });
-    }
-
-    #[test]
-    fn default_sealed_trait() {
-        sealed_typestate!(Side { Client, Server });
-    }
+    fn test_compiles() {}
 }
